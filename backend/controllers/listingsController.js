f476e56ae14eb58a57b7ce8e5214995869497d32
Szymon <szymonwalach@gmail.com>
2022-02-20 16:08:20 +0100

    Error middleware added

--- backend/controllers/listingsController.js
+++ backend/controllers/listingsController.js
@@ -1,17 +1,25 @@
 const Listing = require('../models/listingModel')
+const asyncHandler = require('express-async-handler')
 
-const getListings = async (req, res) => {
-  const listings = await Listing.findOne()
+const getListings = asyncHandler(async (req, res) => {
+  const limit = req.query.limit || 10
+
+  const listings = await Listing.find({}).limit(limit)
 
   res.status(200).json({ listings })
-}
+})
 
-const getOneListing = async (req, res) => {
+const getOneListing = asyncHandler(async (req, res) => {
   const { id } = req.params
   const singleListing = await Listing.findById(id)
 
+  if (!singleListing) {
+    res.status(404)
+    throw new Error('Listing not found')
+  }
+
   res.status(200).json({ singleListing })
-}
+})
 
 module.exports = {
   getListings,
--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,13 @@ const express = require('express')
 const dotenv = require('dotenv').config()
 const PORT = process.env.PORT || 5000
 const connectDB = require('./db/connection')
+const { errorHandler } = require('./middleware/errorMiddleware')
 
 const app = express()
 
 app.use('/api/listings/', require('./routes/listingsRoutes'))
+
+app.use(errorHandler)
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`)
