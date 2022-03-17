5417d27c5556d1bb328ec667badd66bb4f2de482
Szymon <s.walach@soniqsoft.pl>
2022-03-17 15:11:27 +0100

    update

--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -1,33 +1,32 @@
 import React from 'react'
 
 import styles from './Header.module.css'
-import SearchIcon from '@mui/icons-material/Search'
+import MenuIcon from '@mui/icons-material/Menu'
+import CloseIcon from '@mui/icons-material/Close'
 
 const Header = () => {
   return (
     <header className={styles.header}>
       <div className='container'>
         <div className={styles.logo}>
-          rental<span>.</span>io
+          rental<span>.io</span>
         </div>
-        <div className={styles.search}>
-          <div>
-            <input
-              type='text'
-              name='search'
-              id='search'
-              placeholder='Search...'
-            />
-            <SearchIcon className={styles.icon} />
-          </div>
-        </div>
-        <div className={styles.user_details}>
-          <div>
-            <img
-              src='https://t4.ftcdn.net/jpg/03/20/77/17/360_F_320771732_mxVhb6K1K51cM9x5unAsLWwahDCj7s0j.jpg'
-              alt='Profile'
-            />
-            <div>Name</div>
+        <div className={styles.navigation}>
+          <nav>
+            <ul>
+              <li>
+                <a href='#'></a>
+              </li>
+            </ul>
+          </nav>
+          <div className={styles.user_details}>
+            <div>
+              <img
+                src='https://t4.ftcdn.net/jpg/03/20/77/17/360_F_320771732_mxVhb6K1K51cM9x5unAsLWwahDCj7s0j.jpg'
+                alt='Profile'
+              />
+              <div>Name</div>
+            </div>
           </div>
         </div>
       </div>
